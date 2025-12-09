2aeb8caeb9414b90478f9b53ce8c51746ff325ed
ThaKhinSo <usersoe123@gmail.com>
2025-12-09 23:08:09 +0630

    50:21

--- src/main.c
+++ src/main.c
@@ -1,11 +1,16 @@
 #include "raylib.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
 //#if defined(PLATFORM_WEB)
 //    #include <emscripten/emscripten.h>
 //#endif
 
-
+void callback(void* bufferData, unsigned int frames) {
+    int16_t *frames = bufferData;
+    (void)bufferData;
+    //printf("callback(frames = %u)\n", frames);
+}
 //----------------------------------------------------------------------------------
 // Global Variables Definition (local to this module)
 //----------------------------------------------------------------------------------
@@ -32,8 +37,14 @@ int main(void)
 
     InitAudioDevice();
     Music music = LoadMusicStream("ai_kotoba4.mp3");
+    printf("Music framecounts = %u\n", music.frameCount);
+    printf("Music stream sample rate = %u\zn", music.stream.sampleRate);
+    printf("Music stream sample size = %u\n", music.stream.sampleSize);
+    printf("Music stream sample channels = %u\n", music.stream.channels);
+    music.looping = false;
     PlayMusicStream(music);
-    
+    SetMusicVolume(music, 0.5f);
+    AttachAudioStreamProcessor(music.stream, callback);
 //#if defined(PLATFORM_WEB)
 //    emscripten_set_main_loop(UpdateDrawFrame, 60, 1);
 //#else
